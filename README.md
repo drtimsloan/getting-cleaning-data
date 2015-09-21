c237f010824646b84d6580f0c37fcf48c02bd12b
Tim Sloan <drtimsloan@gmail.com>
2015-09-21 20:38:02 +0100

    Edited the README file and uploaded the run_analysis script and CodeBook

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # getting-cleaning-data
 Files for the Getting and Cleaning Data course project
+
+## run_analysis.R
+This script performs the following functions:
+
+1) Downloads the Human Activity Recognition Using Smartphones dataset from the UCI Machine Learning repository.
+The downloaded file is unzipped to a subfolder, which is then set as the new working directory.
+This step can be skipped if the data is already available in the current working directory.
+
+2) Loads the files from the train and test datasets, keeping the variable, activity and subject data in separate tables.
+The activity and feature labels are also loaded in and stored as separate vectors.
+
+3) Merges the test and train data with rbind and then uses a substring search to find all the mean and standard deviation variables.
+As the 'meanFreq' columns are not strictly mean values for each measurement, these are searched separately and removed from the list.
+The final list of variables is then used to subset the dataset.
+
+4) The activity codes are converted to activity labels using the activity label vector. 
+Subject, activity and variables are combined together with cbind into a single data frame. 
+The data frame is reshaped with melt to place all the variables in a single column and
+then recast with only the mean values for each variable by subject and activity. 
+The final tidy dataset is written with write.table.
+
+## CodeBook.md
+This contains information about the data including full descriptions of the variables and how they were calculated.
---
+++ run_analysis.R
@@ -0,0 +1,39 @@
+# Getting and Cleaning Data Course Project
+
+## (Optional) 1) Download the file and change the working directory
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileURL, "UCI_HAR_dataset.zip")
+date_download <- Sys.time()
+unzip("UCI_HAR_dataset.zip")
+setwd("./UCI HAR Dataset")
+
+## 2) Load datasets and label sets
+X_train <- read.table("train/X_train.txt")
+y_train <- read.table("train/y_train.txt")
+subject_train <- read.table("train/subject_train.txt")
+X_test <- read.table("test/X_test.txt")
+y_test <- read.table("test/y_test.txt")
+subject_test <- read.table("test/subject_test.txt")
+features_info <- read.table("features.txt")
+activity_labels <- read.table("activity_labels.txt")
+features <- as.character(features_info$V2)
+
+## 3) Combine the data and subset the columns for mean and std
+X_all <- rbind(X_train, X_test)
+y_all <- rbind(y_train, y_test)
+subject_all <- rbind(subject_train, subject_test)
+mean_freq <- grep("meanFreq",features)
+mean_vec <- grep("mean", features)
+std_vec <- grep("std", features)
+mean_std_vec <- sort(c(mean_vec[!mean_vec %in% mean_freq], std_vec))
+X_subset <- X_all[,mean_std_vec]
+names(X_subset) <- features[mean_std_vec]
+
+## 4) Reshape the data and create a new dataset with variable means by subject and activity
+library(reshape2)
+subject <- as.factor(subject_all$V1)
+activity <- activity_labels[y_all$V1,2]
+full_set <- cbind(subject, activity, X_subset)
+fullMelt <- melt(full_set, id = c("subject","activity"), measure.vars = features[mean_std_vec])
+tidy_set <- dcast(fullMelt, subject + activity ~ variable, mean)
+write.table(tidy_set, file = "tidy_set.txt", row.names = FALSE)
